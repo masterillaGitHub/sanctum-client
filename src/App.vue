4c7182e93aa5b54f19d38d6a90af6e0f05ca5b8d
Police <masterilla@ukr.net>
2023-04-28 16:28:29 +0300

    Add env 'baseURL'

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 axios.defaults.withCredentials = true
 
 async function register() {
